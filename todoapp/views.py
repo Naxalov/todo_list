065d036fb5c1164590e9ad49b7bf090fe28311d2
Diyorbek <diyorbekjumanov1120@gmail.com>
2021-06-26 13:21:45 +0500

    def create

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
+def Add(request):
+    return 0
+
+def Remove(request):
+    return 0
+
+def Update(request):
+    return 0
+
+def Status(request):
+    return 0
+
+def Alltask(request):
+    return 0
 # Create your views here.
