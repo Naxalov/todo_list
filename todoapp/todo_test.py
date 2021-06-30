ca19541320ee77bf50fb54921b8d60fbf042f85b
ZARIF ubuntu <uzbzarif@gmail.com>
2021-06-30 23:29:59 +0900

    get all test

--- todoapp/todo_test.py
+++ todoapp/todo_test.py
@@ -1,7 +1,21 @@
 import requests
-params = {
-	'status':'False',
-	'taskname':'TEST'
-}
-data = requests.get('http://127.0.0.1:8000/add',params=params)
-print(data.json())
+def add_test():
+	params = {
+		'status':'False',
+		'taskname':'TEST'
+	}
+
+	data = requests.get('http://127.0.0.1:8000/add',params=params)
+
+	print(data.json())
+
+
+def get_all_test():
+
+
+	data = requests.get('http://127.0.0.1:8000/get_all')
+
+	print(data.json())
+
+
+get_all_test()
