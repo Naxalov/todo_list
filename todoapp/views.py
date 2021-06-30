dc50172305da520ad3c45d0c170b4464460e3015
ZARIF ubuntu <uzbzarif@gmail.com>
2021-06-30 16:16:07 +0900

    test

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def home(request):
+    print(request.method)
+    print(request.headers)
+    if request.method == "POST":
+        print('POST')
+        print(request.POST)
+    data = {'a':5}
+    return JsonResponse(data)
